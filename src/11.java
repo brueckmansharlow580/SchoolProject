c6c30887aa25cc38f2ee15350c503e9ddf354966
brueckmansharlow580 <brueckmansharlow580@gmail.com>
2025-03-16 05:22:29 +0000

    Add 11.java

---
+++ src/11.java
@@ -0,0 +1,6 @@
+// A simple SchoolProject class that prints a message to the console
+public class SchoolProject {
+  public static void main(String[] args) {
+    System.out.println("Welcome to my school project!");
+  }
+}
