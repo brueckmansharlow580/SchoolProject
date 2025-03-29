27581bcc9d528ac2d83dbcbc389c9637a57e5a89
brueckmansharlow580 <brueckmansharlow580@gmail.com>
2025-03-29 19:05:30 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,6 @@
+public class SimpleSchoolProject {
+    // This is just an example of how to write a simple Java program.
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
