088461f9bfbf23c6ca54e7b3fcdcdb37c1e8588f
brueckmansharlow580 <brueckmansharlow580@gmail.com>
2025-05-24 16:00:48 +0000

    Add 40.java

---
+++ src/40.java
@@ -0,0 +1,6 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("This is my simple Java school project.");
+    }
+}
