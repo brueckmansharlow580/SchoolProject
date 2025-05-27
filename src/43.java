3bb7981808d34e3f5599b6b44278bdaf970d1754
brueckmansharlow580 <brueckmansharlow580@gmail.com>
2025-05-27 04:14:32 +0000

    Add 43.java

---
+++ src/43.java
@@ -0,0 +1,6 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of printing "Hello, World!"
+        System.out.println("Hello, World!");
+    }
+}
